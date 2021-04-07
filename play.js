ca55c5a90e63b9e7dec6660e0aef85ed9a155e29
Lily Habberfield <lilyhabbs@gmail.com>
2021-04-07 19:27:04 +0000

    Set up user interface to handle user input

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+
+const handleUserInput = () => {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+// Set up user interface to handle user input via stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+setupInput();
